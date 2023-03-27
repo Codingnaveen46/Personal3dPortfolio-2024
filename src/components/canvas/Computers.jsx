8a6afbf025fd5e93413e19f20ac64db476183307
Naveen Reddy <110084412+Codingnaveen46@users.noreply.github.com>
2023-03-27 07:54:00 -0700

    rotation changed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.6 : 0.70}
-        position={isMobile ? [0, -2, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
